17bcd4a44392ea9d56d1b9a1abf45ace4625cf60
nljohnson <nathan.luke.johnson@gmail.com>
2020-03-30 23:44:06 -0700

    Bring code more in line with Jeremy's example.
    CODE IS UNTESTED

--- sensor-control/sensor-control.ino
+++ sensor-control/sensor-control.ino
@@ -9,20 +9,56 @@ Serial.begin(9600);
 #endif
 }
 
-int analogPins[6] = { A0, A1, A2, A3, A4, A5 };
-int currentPin = 0;
-int analogValue;
+int buzzLow = 900;      // Define the Lower Limit alarm Frequency.
+int buzzHigh = 2800;    // Define the Upper Limit alarm Frequency.
+
+int pressureLow = 300;  // Define the Lower Limit threshold.
+int pressureHigh = 800; // Define the Upper Limit threshold.
+
+int sensorPin[6] = { A0, A1, A2, A3, A4, A5 };
+int buzzer = 8;         // Select the pin for the Buzzer.
+int ledPin = 13;        // Select the pin for the LED.
+int sensorValue[6];
 void loop() {
   // put your main code here, to run repeatedly:
-  analogValue = analogRead(analogPins[currentPin]);
-  #if DEBUG
-  Serial.print("analog pin ");
-  Serial.print(currentPin);
-  Serial.print(": ");
-  Serial.println(analogValue);
-  if (currentPin == 5) { delay(1000); }
-  #endif
+  checkPressure();
+  for(int i=0; i<6; i=i+1) {
+    #if DEBUG
+    Serial.print("analog pin ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(analogValue);
+    #endif
+    if (sensorValue <= pressureLow) {
+      #if DEBUG
+      Serial.print("Pressure Low on pin ");
+      Serial.println(i);
+      #endif
+      tone(buzzer, buzzLow);
+      digitalWrite(ledPin, HIGH);
+    } else if (sensorValue >= pressureHigh) {
+      #if DEBUG
+      Serial.print("Pressure Low on pin ");
+      Serial.println(i);
+      #endif
+      tone(buzzer, buzzHigh);
+      digitalWrite(ledPin, HIGH);
+    } else {
+      #if DEBUG
+      Serial.print("Pressure OK on pin ");
+      Serial.println(i);
+      #endif
+      notone(buzzer);
+      digitalWrite(ledPin, LOW);
+    }
+    
+    delay(1000);
+  }
 
-  // increment current pin and reset to 0 when it reaches 6.
-  currentPin = (currentPin + 1) % 6;
+}
+
+void checkPressure() {  // Function to read the pressure.
+  for(int x=0; x<6; x=x+1){
+  sensorValue[x] = analogRead(sensorPin[x]);  // read the value from the sensor.
+  }
 }
